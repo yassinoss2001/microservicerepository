8a898c8cb7bb50ce4ea992fe1721d40894afc294
yassinoss2001 <yassine.zitouni@esprit.tn>
2025-02-15 16:27:32 +0100

    repo

---
+++ microserviceproject/src/main/java/tn/esprit/microserviceproject/MicroserviceprojectApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.microserviceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceprojectApplication.class, args);
+	}
+
+}
