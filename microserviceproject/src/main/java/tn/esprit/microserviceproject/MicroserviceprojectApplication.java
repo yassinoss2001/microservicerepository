c12145f0004007a05b3d42357630c490aa4b6f3c
YassineJawadi <126289407+YassineJawadi@users.noreply.github.com>
2025-04-09 23:46:31 +0100

    fixing the MicroserviceprojectApplication

--- microserviceproject/src/main/java/tn/esprit/microserviceproject/MicroserviceprojectApplication.java
+++ microserviceproject/src/main/java/tn/esprit/microserviceproject/MicroserviceprojectApplication.java
@@ -2,8 +2,10 @@ package tn.esprit.microserviceproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroserviceprojectApplication {
 
 	public static void main(String[] args) {
